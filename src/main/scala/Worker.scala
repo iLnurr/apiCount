69131aed26d7c0b0a477132c5c06434f520bc4b2
ilnur serbaev <serbaevilnur@gmail.com>
2017-10-23 14:31:59 +0500

    added http request

--- src/main/scala/Worker.scala
+++ src/main/scala/Worker.scala
@@ -1,4 +1,15 @@
+import java.io.IOException
+
 import akka.actor._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+
+import scala.concurrent.{ExecutionContext, Future}
 
 object Worker {
   def props: Props = Props(new Worker())
@@ -12,6 +23,11 @@ object Worker {
 
 class Worker extends Actor {
   import Worker._
+
+  implicit val system = context.system
+  implicit val ec = context.dispatcher
+  implicit val materializer = ActorMaterializer()
+
   def idle: Receive = {
     case Work(workName) =>
       val master = sender()
@@ -21,7 +37,7 @@ class Worker extends Actor {
 
   def working(workName: String, master: ActorRef): Receive = {
     case Task(wordToCount) =>
-      master ! Result(process(wordToCount))
+      process(wordToCount) map (r => master ! Result(r))
     case WorkDone =>
       context.become(finish(sender()))
   }
@@ -33,6 +49,27 @@ class Worker extends Actor {
 
   override def receive: Receive = idle
 
-  private def process(wordToCount: String): String = s"$wordToCount : 15"
+  private def process(wordToCount: String): Future[String] = {
+    def apiRequest(request: HttpRequest)(implicit system: ActorSystem, ex: ExecutionContext, mat: Materializer): Future[HttpResponse] = {
+      val hostnameApi = "api.stackexchange.com"
+      val portApi = 80
+      val ipApiConnectionFlow: Flow[HttpRequest, HttpResponse, Any] = Http().outgoingConnection(hostnameApi, portApi)
+      Source.single(request).via(ipApiConnectionFlow).runWith(Sink.head)
+    }
+
+    def getInfo(word: String)(implicit system: ActorSystem, ex: ExecutionContext, mat: Materializer): Future[String] = {
+      val linkApi = s"/2.2/search?pagesize=100&order=desc&sort=creation&tagged=${word}&site=stackoverflow"
+      apiRequest(RequestBuilding.Get(linkApi)).flatMap { response ⇒
+        response.status match {
+          case OK ⇒ Unmarshal(response.entity).to[String]
+          case _ ⇒ Unmarshal(response.entity).to[String].flatMap { entity ⇒
+            val error = s"Request failed with status code ${response.status} and entity $entity"
+            Future.failed(new IOException(error))
+          }
+        }
+      }
+    }
+    getInfo(wordToCount)
+  }
 
 }
--- src/test/scala/WorkerSpec.scala
+++ src/test/scala/WorkerSpec.scala
@@ -1,18 +1,22 @@
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
 import org.specs2.mutable.SpecificationLike
+import scala.concurrent.duration._
 
 class WorkerSpec extends TestKit(ActorSystem("worker")) with ImplicitSender with SpecificationLike {
   "Worker" should {
     "correctly procees incoming job" in {
       import Worker._
+
+      val d = 20.second
+
       val worker = system.actorOf(Worker.props)
 
       worker ! Work("findWordCount")
-      expectMsg(GetTask)
+      expectMsg(d, GetTask)
 
-      worker ! Task("odersky")
-      val result = expectMsgType[Result]
+      worker ! Task("clojure")
+      val result = expectMsgType[Result](d)
       println(result)
 
       success
